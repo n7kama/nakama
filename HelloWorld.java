e664f3409ff6a51f9995505dc408743ee33fcab3
n7kama <115482569+n7kama@users.noreply.github.com>
2022-10-10 10:32:59 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+		 public static void main( String[] args )
+		 {
+		 		  // We declare a variable called greeting
+		 		  String greeting = "Hello World";
+
+		 		  // And use it in the print statement
+		 		  System.out.println( greeting );
+		 }
+}
